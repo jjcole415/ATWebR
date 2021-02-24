fd4c75b9f7da4d51af20aa1c887e95f0909cc5e7
Jon Cole <jon.j.cole@gmail.com>
2021-02-24 12:20:28 -0800

    Updates to GetSecurities Functions part 2

--- R/GetSecurities.R
+++ R/GetSecurities.R
@@ -60,22 +60,10 @@ GetSecuritiesList <- function(username, password, enterpriseID){
   file.remove(tmp_call)
 
   doc <- Securities$content %>%
-<<<<<<< HEAD
-    xml2::read_xml()
-||||||| 1319ef9
-    read_xml()
-=======
     xml2::read_xml()
 
->>>>>>> f63fc1388937431b93fb523f5dee78eb9329c76a
-
-<<<<<<< HEAD
-
   #################################
-||||||| 1319ef9
-=======
-  #################################
->>>>>>> f63fc1388937431b93fb523f5dee78eb9329c76a
+
   (securities_data <- doc %>%
      xml2::xml_find_all('.//b:GetSecuritiesListSecurity', ns = xml2::xml_ns(doc)))
 
@@ -105,7 +93,8 @@ GetSecuritiesList <- function(username, password, enterpriseID){
       dplyr::select(-SecurityClasses) %>%
       tidyr::unnest(cols = c(SecurityMultiplier, SecurityName, SecurityNotes, SecurityPrimaryContactProfileID,
                              SecurityReportingCurrency, SecuritySecondaryContactProfileID,
-                             SecurityType, SecurityTypeID, SecurityTypeName))
+                             SecurityType, SecurityTypeID, SecurityTypeName, SecurityIdentifierName,
+                             SecurityIdentifierPrimary, SecurityIdentifierValue))
   )
 
   # (securities_df <- securities_rows %>%
@@ -237,7 +226,6 @@ GetSecurityClasses <- function(username, password, enterpriseID){
   ATWeb_Logout(username = username, password = password, SessionID = SessionID)
   file.remove(tmp_call)
 
-<<<<<<< HEAD
 
   #################################
   doc <- Securities$content %>%
@@ -365,185 +353,10 @@ GetSecurityClasses <- function(username, password, enterpriseID){
   #     EffectiveDate == max(EffectiveDate) ~ "FALSE",
   #     TRUE ~ "TRUE"
   #   ))
-||||||| 1319ef9
-  securities_result <- Securities$content %>%
-    xml2::read_xml() %>% xml2::as_list()
-  securities_list <- securities_result$Envelope$Body$GetSecuritiesListResponse$GetSecuritiesListResult$Securities
-  securities_df <- tibble(securities = securities_list) %>%
-    tidyr::unnest_wider(securities) %>%
-    tidyr::unnest_longer(SecurityClasses) %>%
-    tidyr::unnest_wider(SecurityClasses) %>%
-    tidyr::unnest_longer(SecurityClassCode              ) %>%
-    tidyr::unnest_longer(SecurityClassEffectiveDate     ) %>%
-    tidyr::unnest_longer(SecurityClassID                ) %>%
-    tidyr::unnest_longer(SecurityClassName              ) %>%
-    tidyr::unnest_longer(SecurityClassNotes             ) %>%
-    tidyr::unnest_longer(SecurityClassOptions           ) %>%
-    dplyr::select(-...1) %>%
-    tidyr::unnest_wider(SecurityClassOptions           ) %>%
-    tidyr::unnest_longer(SecurityClassOptionCode        ) %>%
-    tidyr::unnest_longer(SecurityClassOptionID          ) %>%
-    tidyr::unnest_longer(SecurityClassOptionName        ) %>%
-    tidyr::unnest_longer(CashOrEquivalent) %>%
-    tidyr::unnest_longer(NotionalValue) %>%
-    tidyr::unnest_longer(RecordIncomeAccruals) %>%
-    tidyr::unnest_longer(SecurityDefaultTransactionType) %>%
-    tidyr::unnest_longer(SecurityDefaultTransactionType) %>%
-    tidyr::unnest_longer(SecurityID) %>%
-    tidyr::unnest_longer(SecurityIdentifiers) %>%
-    tidyr::unnest_wider(SecurityIdentifiers) %>%
-    tidyr::unnest_longer(SecurityIdentifierName) %>%
-    tidyr::unnest_longer(SecurityIdentifierPrimary) %>%
-    tidyr::unnest_longer(SecurityIdentifierValue) %>%
-    tidyr::unnest_longer(SecurityMultiplier) %>%
-    tidyr::unnest_longer(SecurityName) %>%
-    tidyr::unnest_longer(SecurityType) %>%
-    tidyr::unnest_longer(SecurityTypeID) %>%
-    tidyr::unnest_longer(SecurityTypeName) %>%
-    tidyr::unnest_longer(SecurityReportingCurrency) %>%
-    tidyr::unnest_longer(SecurityNotes) %>%
-    dplyr::filter(SecurityIdentifierPrimary == "true")   %>%
-    dplyr::select(PrimaryIdentifier = SecurityIdentifierValue,
-           SecurityID,
-           SecurityName,
-           SecurityType = SecurityTypeName,
-           SecurityClass= SecurityClassName,
-           SecurityClassOption = SecurityClassOptionName,
-           EffectiveDate = SecurityClassEffectiveDate
-    ) %>%
-    readr::type_convert() %>%
-    dplyr::group_by(PrimaryIdentifier, SecurityClass) %>%
-    dplyr::mutate(HistoricalSetting = case_when(
-      is.na(SecurityClassOption) ~ "",
-      EffectiveDate == max(EffectiveDate) ~ "FALSE",
-      TRUE ~ "TRUE"
-    ))
-=======
+
 
   #################################
-  (securities_data <- doc %>%
-     xml2::xml_find_all('.//b:GetSecuritiesListSecurity', ns = xml2::xml_ns(doc)))
 
-  (securities_rows <- tibble(
-    row = seq_along(securities_data),
-    CashOrEquivalent = securities_data %>%  map(~ xml2::xml_find_all(.x, './/b:CashOrEquivalent', ns = xml2::xml_ns(doc))) %>% map(~ xml_text(.x)),
-    NotionalValue = securities_data %>%  map(~ xml2::xml_find_all(.x, './/b:NotionalValue', ns = xml2::xml_ns(doc))) %>% map(~ xml_text(.x)),
-    RecordIncomeAccruals = securities_data %>%  map(~ xml2::xml_find_all(.x, './/b:RecordIncomeAccruals', ns = xml2::xml_ns(doc))) %>% map(~ xml_text(.x)),
-    SecurityClasses = securities_data %>%  map(~ xml2::xml_find_all(.x, './/b:GetSecuritiesListSecurityClass', ns = xml2::xml_ns(doc))) %>% map(~ as_list(.x)) %>% map(~ tibble(data =.x)) %>% map( ~ unnest_wider(data = .x, col = data)) %>% map( ~ unnest(.x, )),
-    SecurityDefaultTransactionType = securities_data %>%  map(~ xml2::xml_find_all(.x, './/b:SecurityDefaultTransactionType', ns = xml2::xml_ns(doc))) %>% map(~ xml_text(.x)),
-    SecurityID = securities_data %>%  map(~ xml2::xml_find_all(.x, './/b:SecurityID', ns = xml2::xml_ns(doc))) %>% map(~ xml_text(.x)),
-    SecurityMultiplier = securities_data %>%  map(~ xml2::xml_find_all(.x, './/b:SecurityMultiplier', ns = xml2::xml_ns(doc))) %>% map(~ xml_text(.x)),
-    SecurityName = securities_data %>%  map(~ xml2::xml_find_all(.x, './/b:SecurityName', ns = xml2::xml_ns(doc))) %>% map(~ xml_text(.x)),
-    SecurityNotes = securities_data %>%  map(~ xml2::xml_find_all(.x, './/b:SecurityNotes', ns = xml2::xml_ns(doc))) %>% map(~ xml_text(.x)),
-    SecurityPrimaryContactProfileID = securities_data %>%  map(~ xml2::xml_find_all(.x, './/b:SecurityPrimaryContactProfileID', ns = xml2::xml_ns(doc))) %>% map(~ xml_text(.x)),
-    SecurityReportingCurrency = securities_data %>%  map(~ xml2::xml_find_all(.x, './/b:SecurityReportingCurrency', ns = xml2::xml_ns(doc))) %>% map(~ xml_text(.x)),
-    SecuritySecondaryContactProfileID = securities_data %>%  map(~ xml2::xml_find_all(.x, './/b:SecuritySecondaryContactProfileID', ns = xml2::xml_ns(doc))) %>% map(~ xml_text(.x)),
-    SecurityType = securities_data %>%  map(~ xml2::xml_find_all(.x, './/b:SecurityType', ns = xml2::xml_ns(doc))) %>% map(~ xml_text(.x)),
-    SecurityTypeID = securities_data %>%  map(~ xml2::xml_find_all(.x, './/b:SecurityTypeID', ns = xml2::xml_ns(doc))) %>% map(~ xml_text(.x)),
-    SecurityTypeName = securities_data %>%  map(~ xml2::xml_find_all(.x, './/b:SecurityTypeName', ns = xml2::xml_ns(doc))) %>% map(~ xml_text(.x)),
-    SecurityIdentifiers = securities_data %>%  map(~ xml2::xml_find_all(.x, './/b:GetSecuritiesListSecurityIdentifier', ns = xml2::xml_ns(doc))) %>% map(~ as_list(.x)) %>% map(~ tibble(data =.x)) %>% map( ~ unnest_wider(data = .x, col = data)) %>% map( ~ unnest(.x, cols = c(SecurityIdentifierName, SecurityIdentifierPrimary, SecurityIdentifierValue)))
-  ))
-
-  (securities_df <- securities_rows %>%
-      tidyr::unnest(cols = c(SecurityIdentifiers)) %>%
-      tidyr::unnest(cols = c(CashOrEquivalent, NotionalValue, RecordIncomeAccruals, SecurityClasses,
-                             SecurityDefaultTransactionType, SecurityID, SecurityMultiplier,
-                             SecurityName, SecurityNotes, SecurityPrimaryContactProfileID,
-                             SecurityReportingCurrency, SecuritySecondaryContactProfileID,
-                             SecurityType, SecurityTypeID, SecurityTypeName, SecurityIdentifierName,
-                             SecurityIdentifierPrimary, SecurityIdentifierValue)) %>%
-      tidyr::unnest_wider(SecurityClassOptions) %>%
-      tidyr::unnest(cols = c(SecurityClassCode, SecurityClassEffectiveDate, SecurityClassID,
-                             SecurityClassName, SecurityClassNotes, SecurityClassOptionCode,
-                             SecurityClassOptionID, SecurityClassOptionName)) %>%
-      dplyr::filter(SecurityIdentifierPrimary == "true")   %>%
-      dplyr::select(PrimaryIdentifier = SecurityIdentifierValue,
-                    SecurityID,
-                    SecurityName,
-                    SecurityType = SecurityTypeName,
-                    SecurityClass= SecurityClassName,
-                    SecurityClassOption = SecurityClassOptionName,
-                    EffectiveDate = SecurityClassEffectiveDate
-      ) %>%
-      unnest(cols = c(SecurityClassOption)) %>%
-      readr::type_convert(cols(
-        PrimaryIdentifier = col_character(),
-        SecurityID = col_double(),
-        SecurityName = col_character(),
-        SecurityType = col_character(),
-        SecurityClass = col_character(),
-        EffectiveDate = col_datetime(format = "")
-      )) %>%
-      dplyr::group_by(PrimaryIdentifier, SecurityClass) %>%
-      dplyr::mutate(HistoricalSetting = case_when(
-        is.na(SecurityClassOption) ~ "",
-        EffectiveDate == max(EffectiveDate) ~ "FALSE",
-        TRUE ~ "TRUE"
-      )
-  ))
-  #################################
-
-  # securities_result <- Securities$content %>%
-  #   xml2::read_xml() %>% xml2::as_list()
-  # securities_list <- securities_result$Envelope$Body$GetSecuritiesListResponse$GetSecuritiesListResult$Securities
-  # securities_df <- tibble(securities = securities_list) %>%
-  #   tidyr::unnest_wider(securities) %>%
-  #   tidyr::unnest_longer(SecurityClasses) %>%
-  #   tidyr::unnest_wider(SecurityClasses) %>%
-  #   tidyr::unnest_longer(SecurityClassCode              ) %>%
-  #   tidyr::unnest_longer(SecurityClassEffectiveDate     ) %>%
-  #   tidyr::unnest_longer(SecurityClassID                ) %>%
-  #   tidyr::unnest_longer(SecurityClassName              ) %>%
-  #   tidyr::unnest_longer(SecurityClassNotes             ) %>%
-  #   tidyr::unnest_longer(SecurityClassOptions           ) %>%
-  #   dplyr::select(-...1) %>%
-  #   tidyr::unnest_wider(SecurityClassOptions           ) %>%
-  #   tidyr::unnest_longer(SecurityClassOptionCode        ) %>%
-  #   tidyr::unnest_longer(SecurityClassOptionID          ) %>%
-  #   tidyr::unnest_longer(SecurityClassOptionName        ) %>%
-  #   tidyr::unnest_longer(CashOrEquivalent) %>%
-  #   tidyr::unnest_longer(NotionalValue) %>%
-  #   tidyr::unnest_longer(RecordIncomeAccruals) %>%
-  #   tidyr::unnest_longer(SecurityDefaultTransactionType) %>%
-  #   tidyr::unnest_longer(SecurityDefaultTransactionType) %>%
-  #   tidyr::unnest_longer(SecurityID) %>%
-  #   tidyr::unnest_longer(SecurityIdentifiers) %>%
-  #   tidyr::unnest_wider(SecurityIdentifiers) %>%
-  #   tidyr::unnest_longer(SecurityIdentifierName) %>%
-  #   tidyr::unnest_longer(SecurityIdentifierPrimary) %>%
-  #   tidyr::unnest_longer(SecurityIdentifierValue) %>%
-  #   tidyr::unnest_longer(SecurityMultiplier) %>%
-  #   tidyr::unnest_longer(SecurityName) %>%
-  #   tidyr::unnest_longer(SecurityType) %>%
-  #   tidyr::unnest_longer(SecurityTypeID) %>%
-  #   tidyr::unnest_longer(SecurityTypeName) %>%
-  #   tidyr::unnest_longer(SecurityReportingCurrency) %>%
-  #   tidyr::unnest_longer(SecurityNotes) %>%
-  #   dplyr::filter(SecurityIdentifierPrimary == "true")   %>%
-  #   dplyr::select(PrimaryIdentifier = SecurityIdentifierValue,
-  #          SecurityID,
-  #          SecurityName,
-  #          SecurityType = SecurityTypeName,
-  #          SecurityClass= SecurityClassName,
-  #          SecurityClassOption = SecurityClassOptionName,
-  #          EffectiveDate = SecurityClassEffectiveDate
-  #   ) %>%
-  #   readr::type_convert(cols(
-  #     PrimaryIdentifier = col_character(),
-  #     SecurityID = col_double(),
-  #     SecurityName = col_character(),
-  #     SecurityType = col_character(),
-  #     SecurityClass = col_character(),
-  #     SecurityClassOption = col_character(),
-  #     EffectiveDate = col_datetime(format = "")
-  #   )) %>%
-  #   dplyr::group_by(PrimaryIdentifier, SecurityClass) %>%
-  #   dplyr::mutate(HistoricalSetting = case_when(
-  #     is.na(SecurityClassOption) ~ "",
-  #     EffectiveDate == max(EffectiveDate) ~ "FALSE",
-  #     TRUE ~ "TRUE"
-  #   ))
->>>>>>> f63fc1388937431b93fb523f5dee78eb9329c76a
 
   return(securities_df)
 
